3d9e051f9e0ee351c9a4ce9453d141f0ea059fec
Perry Trinh <perry.trinh@berkeley.edu>
2017-01-21 03:04:00 -0800

    Fixed closest_time to correctly account for minutes, added cut_zeros class

--- alarm.py
+++ alarm.py
@@ -30,12 +30,11 @@ def main():
                 alarm_time = convertTime(user_input, index_last_number, False)
                 break
 
+        current_time = time.strftime("%H:%M")
+
         #If user forgot AM/PM, assume time is same time period as right now
         if alarm_time == "":
-            current_hour = int(time.strftime("%H"))
-            alarm_time = closest_time(user_input, current_hour, index_last_number)
-
-        current_time = time.strftime("%H:%M")
+            alarm_time = closest_time(user_input, current_time, index_last_number)
 
         while current_time != alarm_time:
             # Every 10 seconds, the current time is displayed
@@ -68,23 +67,37 @@ def convertTime(input, index, PM):
     else:
         return input[:index + 1]
 
-def closest_time(input, current_hour, index):
+def cut_zeros(time, time_period):
+    if time_period == "Minutes":
+        return int(time[4:5]) if time[3] == "0" else int(time[3:])
+    else:
+        return int(time[1:2]) if time[0] == "0" else int(time[:2])
+
+def closest_time(input, current_time, index):
     """Determines what time is closest to current_time and returns it as a String"""
     """ex. if current_time is 14:40 PM, and user inputs 02:45, closest time is 14:45"""
 
     AM_time = convertTime(input, index, False)
     PM_time = convertTime(input, index, True)
 
-    #Corrects for single digit hours so they can be compared with later on
-    AM_hours = int(AM_time[1:2]) if AM_time[0] == "0" else int(AM_time[:2])
-    PM_hours = int(PM_time[1:2]) if PM_time[0] == "0" else int(PM_time[:2])
+    #Corrects for single digit time stamps so they can be compared with later on
+    AM_hours = cut_zeros(AM_time, "Hours")
+    PM_hours = cut_zeros(PM_time, "Hours")
+    current_hour = cut_zeros(current_time, "Hours")
+    AM_min = cut_zeros(AM_time, "Minutes")
+    PM_min = cut_zeros(PM_time, "Minutes")
+    current_minute = cut_zeros(current_time, "Minutes")
 
-    if PM_hours == AM_hours and AM_hours == current_hour:
-        if int(current_hour) >= 12:
+    if current_hour == AM_hours:
+        if AM_min - current_minute < 0:
             return PM_time
         else:
             return AM_time
-
+    elif current_hour == PM_hours:
+        if PM_min - current_minute < 0:
+            return AM_time
+        else:
+            return PM_time
     elif PM_hours >= current_hour and current_hour > AM_hours:
         return PM_time
     else:
