86a5133fabf3070ebff061c17c4858030b8490b2
Perry Trinh <perry.trinh@berkeley.edu>
2017-01-21 02:30:45 -0800

    Added closest_time method and some comments

--- alarm.py
+++ alarm.py
@@ -32,12 +32,8 @@ def main():
 
         #If user forgot AM/PM, assume time is same time period as right now
         if alarm_time == "":
-            current_hour = time.strftime("%H")
-
-            if int(current_hour) >= 12:
-                alarm_time = convertTime(user_input, index_last_number, True)
-            else:
-                alarm_time = convertTime(user_input, index_last_number, False)
+            current_hour = int(time.strftime("%H"))
+            alarm_time = closest_time(user_input, current_hour, index_last_number)
 
         current_time = time.strftime("%H:%M")
 
@@ -65,10 +61,34 @@ def is_numeric(t):
         return False
 
 def convertTime(input, index, PM):
+    """Returns input in military time as a String"""
     if PM == True:
         alarm_hours = int(input[0:2]) + 12
         return str(alarm_hours) + input[2:index + 1]
     else:
         return input[:index + 1]
+
+def closest_time(input, current_hour, index):
+    """Determines what time is closest to current_time and returns it as a String"""
+    """ex. if current_time is 14:40 PM, and user inputs 02:45, closest time is 14:45"""
+
+    AM_time = convertTime(input, index, False)
+    PM_time = convertTime(input, index, True)
+
+    #Corrects for single digit hours so they can be compared with later on
+    AM_hours = int(AM_time[1:2]) if AM_time[0] == "0" else int(AM_time[:2])
+    PM_hours = int(PM_time[1:2]) if PM_time[0] == "0" else int(PM_time[:2])
+
+    if PM_hours == AM_hours and AM_hours == current_hour:
+        if int(current_hour) >= 12:
+            return PM_time
+        else:
+            return AM_time
+
+    elif PM_hours >= current_hour and current_hour > AM_hours:
+        return PM_time
+    else:
+        return AM_time
+
 
 main()
